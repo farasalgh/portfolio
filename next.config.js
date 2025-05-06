2abc146b1e75c31976357ae4e08bdf8c0098dbca
Faras <nvsrfaras@gmail.com>
2025-05-06 10:57:07 +0700

    chore: update Next.js config to ignore ESLint during builds

--- next.config.js
+++ next.config.js
@@ -1,9 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['raw.githubusercontent.com'],
-  },
-}
-
-module.exports = nextConfig 
+ 
